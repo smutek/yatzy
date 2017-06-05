a0fd3774b93395d5b3327211d85eb40b71c6fc43
Jimmy Smutek <jimmy@smutek.net>
2017-06-04 21:03:43 -0400

    Scoresheet

--- hands.py
+++ hands.py
@@ -15,6 +15,13 @@ class Hand(list):
         # sort dice in hand
         self.sort()
 
+    def _by_value(self, value):
+        dice = []
+        for die in self:
+            if die == value:
+                dice.append(die)
+            return dice
+
 
 # Extend Hand to simulate a Yatzy specific die roll
 class YatzyHand(Hand):
@@ -22,3 +29,37 @@ class YatzyHand(Hand):
         # Init with  5 six sided die
         super().__init__(size=5, die_class=D6, *args, **kwargs)
 
+    @property
+    def ones(self):
+        return self._by_value(1)
+
+    @property
+    def twos(self):
+        return self._by_value(2)
+
+    @property
+    def threes(self):
+        return self._by_value(3)
+
+    @property
+    def fours(self):
+        return self._by_value(4)
+
+    @property
+    def fives(self):
+        return self._by_value(5)
+
+    @property
+    def sixes(self):
+        return self._by_value(6)
+
+    @property
+    def _sets(self):
+        return {
+            1: len(self.ones),
+            2: len(self.twos),
+            3: len(self.threes),
+            4: len(self.fours),
+            5: len(self.fives),
+            6: len(self.sixes),
+        }
